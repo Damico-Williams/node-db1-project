a8c485d896a895e0d7a0a225c449c4c371b5d582
Damico Williams <damicowilliams@gmail.com>
2020-08-19 19:23:16 -0700

    p

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const server = require("./api/server.js");
+const Apiserver = require("./api/Apiserver.js");
+const express = require("express");
 
+
+Apiserver.use(express.json());
 const PORT = process.env.PORT || 5000;
+
+
+const server = express();
+server.use(Apiserver)
+
+
 
 server.listen(PORT, () => {
   console.log(`\n== API running on port ${PORT} ==\n`);
